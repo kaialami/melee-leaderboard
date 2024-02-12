ade14f45337d48426d1aa98c8d538bc510f170e2
kaialami <kaikaialami@gmail.com>
2024-02-11 18:34:19 -0800

    dev get request testing

--- server/server.js
+++ server/server.js
@@ -8,6 +8,18 @@ app.get("/players", async (req, res) => {
     res.send(players);
 });
 
+app.get("/dev", (req, res) => {
+    res.send([{
+        id: 'dev',
+        username: 'dev',
+        ranking: 23,
+        elo: 2000,
+        wins: 10,
+        played: 22,
+        visible: 1
+    }]);
+})
+
 app.use(express.static("public"));
 
 const port = 8080;
