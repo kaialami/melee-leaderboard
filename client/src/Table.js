33eb0fbf663e7a0f0af7ece28529994e5a48d82a
kaialami <kaikaialami@gmail.com>
2024-02-11 18:34:37 -0800

    dev get request testing

--- client/src/Table.js
+++ client/src/Table.js
@@ -3,39 +3,45 @@ import TableHeader from "./TableHead.js";
 
 const Table = () => {
     const startgg = "https://start.gg/user/";
+    const pathname = window.location.pathname;
     const [players, setPlayers] = useState([]);
 
     useEffect(() => {
-        fetch("/players").then(res => res.json()).then( data => {
-            setPlayers(data);
+        let req = "/players"
+        if (pathname === "/dev") {
+            req = pathname;
+        }
+
+        fetch(req).then(res => res.json()).then(data => {
+            setPlayers(data)
         })
     }, []);
 
+    if (players.length === 0) return <p>Loading...</p>;
+
     return (
         <div>
-            {(players.length === 0) ? (<p>Loading...</p>): (
-                <table className="leaderboard">
-                    <TableHeader />
-                    <tbody>
-                        {players.map(player => {
-                            if (player.visible === 0) return <tr key={player.id}></tr>;
+            <table className="leaderboard">
+                <TableHeader />
+                <tbody>
+                    {players.map(player => {
+                        if (player.visible === 0) return <tr key={player.id}></tr>;
 
-                            let ranking = player.ranking;
-                            if (ranking == null) ranking = "n/a";
-                            return (
-                                <tr key={player.id}>
-                                    <td>{ranking}</td>
-                                    {/* eslint-disable-next-line */}
-                                    <td className="player"><a href={startgg + player.id} target="_blank">{player.username}</a></td>
-                                    <td>{player.elo}</td>
-                                    <td className="sets">{player.wins}</td>
-                                    <td className="sets">{player.played}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            )}
+                        let ranking = player.ranking;
+                        if (ranking == null) ranking = "";
+                        return (
+                            <tr key={player.id}>
+                                <td>{ranking}</td>
+                                {/* eslint-disable-next-line */}
+                                <td className="player"><a href={startgg + player.id} target="_blank">{player.username}</a></td>
+                                <td>{player.elo}</td>
+                                <td className="sets">{player.wins}</td>
+                                <td className="sets">{player.played}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div> 
     );
 }
