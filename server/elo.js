a6932f537e02ad54cb846928a2887878e531d291
kaialami <kaikaialami@gmail.com>
2024-02-12 17:17:26 -0800

    calculate elo uses a weight factor to multiply change by

--- server/elo.js
+++ server/elo.js
@@ -7,12 +7,16 @@ const lowest = 1000;
  * 
  * Elo changes by half for losers bracket sets.
  * 
+ * Weight increases elo changes by a factor of the weight. 
+ * Higher-stakes tournaments (e.g. air series) should use higher weights (2 seems good).
+ * 
  * https://stanislav-stankovic.medium.com/elo-rating-system-6196cc59941e 
  * 
  * @param {*} ra Rating of player A
  * @param {*} rb Rating of player B
  * @param {*} outcome 1 if A won, 0 if A lost
  * @param {*} bracket 1 if winners bracket, 2 if losers
+ * @param {*} weight factor to multiply change by
  */
 export function calculate(ra, rb, outcome, bracket, weight) {
     let expected = (10 ** (ra / c)) / (10 ** (ra / c) + 10 ** (rb / c));
