86efa0f6ee9348e3faa4d4d4ab967b665186c36d
kaialami <kaikaialami@gmail.com>
2024-02-12 17:16:50 -0800

    go back page

--- client/src/PlayerDetails.js
+++ client/src/PlayerDetails.js
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
+
 const PlayerDetails = ({ player }) => {
     const startgg = "https://start.gg/user/";
     
     return (  
         <div className="player-details">
             <h1>{player.username}</h1>
-            {/* eslint-disable-next-line */}
-            <a href={startgg + player.id} target="_blank">start.gg</a>
+            <div className="startgg-link">
+                {/* eslint-disable-next-line */}
+                <a href={startgg + player.id} target="_blank">start.gg</a>
+            </div>
+            <Link to="/">go back</Link>
         </div>
     );
 }
